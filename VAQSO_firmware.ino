467e731509271f88a127e1476ce4c68b9d2a96bc
taketake25 <takeru31415@gmail.com>
2020-12-31 20:08:06 +0900

    add main code

--- VAQSO_firmware.ino
+++ VAQSO_firmware.ino
@@ -11,100 +11,133 @@ int sumOfHighFan = 0;
 char input = 0;
 int index = 0;
 
-
-void setFans(int fanVal){
-  for(int i=0;i<5;i++){
-    fanStatus[i]=1;
-  }
-  return;  
-}
-
-void onOnlyAFan(int fanNumber){
-//  Serial.println(fanNumber);
-  for(int i = 0; i < 5; i++){
-    if(i == fanNumber)digitalWrite(fans[i],HIGH);
-    else digitalWrite(fans[i],LOW);
+void setFans(int fanVal)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    fanStatus[i] = 1;
   }
   return;
 }
 
-void resetFans(){
-  for(int i=0;i<5;i++){
-    fanStatus[i]=0;
-    digitalWrite(fans[i],LOW);
+void onOnlyAFan(int fanNumber)
+{
+  //  Serial.println(fanNumber);
+  for (int i = 0; i < 5; i++)
+  {
+    if (i == fanNumber)
+      analogWrite(fans[i], (int)255 / 2);
+    else
+      analogWrite(fans[i], 0);
+  }
+  return;
+}
+
+void resetFans()
+{
+  for (int i = 0; i < 5; i++)
+  {
+    fanStatus[i] = 0;
+    analogWrite(fans[i], 0);
   }
   sumOfHighFan = 0;
 
   return;
 }
 
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+  Serial.begin(115200);
   pinMode(fan0, OUTPUT);
   pinMode(fan1, OUTPUT);
   pinMode(fan2, OUTPUT);
   pinMode(fan3, OUTPUT);
   pinMode(fan4, OUTPUT);
-  digitalWrite(fan0, LOW);
-  digitalWrite(fan1, LOW);
-  digitalWrite(fan2, LOW);
-  digitalWrite(fan3, LOW);
-  digitalWrite(fan4, LOW);
+  analogWrite(fan0, 0);
+  analogWrite(fan1, 0);
+  analogWrite(fan2, 0);
+  analogWrite(fan3, 0);
+  analogWrite(fan4, 0);
   delay(1000);
 }
 
-void loop() {
-  if(Serial.available() > 0){
+void loop()
+{
+  if (Serial.available() > 0)
+  {
     index = 0;
-    memset(inputCommand,0,5); // 入力コマンドの初期化
+    memset(inputCommand, 0, 5); // 入力コマンドの初期化
 
-    while(Serial.available() > 0){  
-      input = 'A' + Serial.read()-65; // char型として1byte読み込む
+    while (Serial.available() > 0)
+    {
+      input = 'A' + Serial.read() - 65; // char型として1byte読み込む
       inputCommand[index] = input;
-      
-      if(index >= 5)continue; // コマンドの文字数制限
-      if(input == '\r' || input == '\n' || input == ' ')continue;
+
+      if (index >= 5)
+        continue; // コマンドの文字数制限
+      if (input == '\r' || input == '\n' || input == ' ')
+        continue;
       index++;
     }
     Serial.println(inputCommand);
-    
-    if(index < 5){Serial.println("too short length");} // out of indexを避ける
-    else if(inputCommand[0]=='f'){ // fan10やfan11など
-      int fanNum = int(inputCommand[3]-'0');
-      int fanVal = int(inputCommand[4]-'0');
+
+    if (index < 5)
+    {
+      Serial.println("too short length");
+    } // out of indexを避ける
+    else if (inputCommand[0] == 'f')
+    { // fan10やfan11など
+      int fanNum = int(inputCommand[3] - '0');
+      int fanVal = int(inputCommand[4] - '0');
       int previousVal = fanStatus[fanNum];
-      
-      if(previousVal != fanVal){ // 以前に格納されていた値と違った場合のみsumやstatusを変更
-        if (fanVal == 1)sumOfHighFan++; 
-        else if(fanVal == 0)sumOfHighFan--;
+
+      if (previousVal != fanVal)
+      { // 以前に格納されていた値と違った場合のみsumやstatusを変更
+        if (fanVal == 1)
+          sumOfHighFan++;
+        else if (fanVal == 0)
+        {
+          sumOfHighFan--;
+          analogWrite(fans[fanNum], 0);
+        }
 
         fanStatus[fanNum] = fanVal;
-        Serial.write("fun");
-        Serial.write(fanNum);
+        Serial.write("fan");
+        Serial.print(fanNum);
         Serial.println(" is changed");
       }
-    } else if(inputCommand[0] == 'r'){ // ファンの状態をすべて初期化する
+    }
+    else if (inputCommand[0] == 'r')
+    { // ファンの状態をすべて初期化する
       resetFans();
       Serial.println("status is reset");
-    } else if(inputCommand[0] == 'a'){
-      int fanNum = int(inputCommand[4]-'0');
-      if(fanNum == 0){resetFans();}
-      else if(fanNum == 1){
+    }
+    else if (inputCommand[0] == 'a')
+    {
+      int fanNum = int(inputCommand[4] - '0');
+      if (fanNum == 0)
+      {
+        resetFans();
+      }
+      else if (fanNum == 1)
+      {
         setFans(HIGH);
         sumOfHighFan = 5;
         Serial.println("all turned on");
       }
     }
 
-    for(int i=0; i<5;i++){
-      Serial.print(fanStatus[i]);
+    for (int i = 0; i < 5; i++)
+    {
+      Serial.print(fanStatus[i]); 
     }
   }
 
-
-  int onTimeMS = (int)100/sumOfHighFan; // 割り込み処理として書き直したい
-  for(int i=0; i<5; i++){ // HIGHの個数に合わせた長さでファンを動作
-    if(fanStatus[i] == 1){
+  int onTimeMS = (int)100 / sumOfHighFan; // 割り込み処理として書き直したい
+  for (int i = 0; i < 5; i++)
+  { // HIGHの個数に合わせた長さでファンを動作
+    if (fanStatus[i] == 1)
+    {
       onOnlyAFan(i);
       delay(onTimeMS);
     }
